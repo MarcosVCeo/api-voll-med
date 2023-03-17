b800d7acf1584a349119bf49303e09c1d9d220ae
Marcos Vinicius Ceola <marcos.vc321@gmail.com>
2023-03-16 22:59:10 -0300

    Adição de novo end point

--- src/main/java/br/com/marcosceola/api/controller/MedicoController.java
+++ src/main/java/br/com/marcosceola/api/controller/MedicoController.java
@@ -1,15 +1,17 @@
 package br.com.marcosceola.api.controller;
 
+import br.com.marcosceola.api.dto.EnderecoForm;
 import br.com.marcosceola.api.dto.MedicoForm;
 import br.com.marcosceola.api.model.Endereco;
 import br.com.marcosceola.api.model.Medico;
 import br.com.marcosceola.api.service.MedicoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,8 +21,20 @@ public class MedicoController {
     private MedicoService medicoService;
 
     @PostMapping
+    @Transactional
     public void cadastrar(@Valid @RequestBody MedicoForm medicoForm) {
         medicoService.save(medicoForm2Medico(medicoForm));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MedicoForm>> listar() {
+        var medicosForm = medicoService
+                .listAll()
+                .stream()
+                .map(MedicoController::medico2MedicoForm)
+                .toList();
+
+        return ResponseEntity.ok(medicosForm);
     }
 
     private static Medico medicoForm2Medico(MedicoForm medicoForm) {
@@ -34,12 +48,35 @@ public class MedicoController {
                 enderecoForm.uf(),
                 enderecoForm.cep());
 
-        return new Medico(
+        var medico = new Medico(
                 null,
                 medicoForm.nome(),
                 medicoForm.email(),
                 medicoForm.crm(),
                 medicoForm.especialidade(),
                 endereco);
+
+        return medico;
+    }
+
+    private static MedicoForm medico2MedicoForm(Medico medico) {
+        var endereco = medico.getEndereco();
+        var enderecoForm = new EnderecoForm(
+                endereco.getLogradouro(),
+                endereco.getNumero(),
+                endereco.getComplemento(),
+                endereco.getBairro(),
+                endereco.getCidade(),
+                endereco.getUf(),
+                endereco.getCep());
+
+        var medicoForm = new MedicoForm(
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getEspecialidade(),
+                enderecoForm);
+
+        return medicoForm;
     }
 }
--- src/main/java/br/com/marcosceola/api/service/MedicoService.java
+++ src/main/java/br/com/marcosceola/api/service/MedicoService.java
@@ -5,6 +5,9 @@ import br.com.marcosceola.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 public class MedicoService {
 
@@ -13,5 +16,9 @@ public class MedicoService {
 
     public void save(Medico medico) {
         repository.save(medico);
+    }
+
+    public List<Medico> listAll() {
+        return repository.findAll();
     }
 }
