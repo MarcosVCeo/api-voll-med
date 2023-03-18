fac6d0a15bda99c767d36114d7eb27a07584a060
Marcos Vinicius Ceola <marcos.vc321@gmail.com>
2023-03-18 13:44:41 -0300

    Ajuste em nova db migration

--- src/main/java/br/com/marcosceola/api/controller/PacienteController.java
+++ src/main/java/br/com/marcosceola/api/controller/PacienteController.java
@@ -4,7 +4,7 @@ import br.com.marcosceola.api.dto.paciente.PacienteForm;
 import br.com.marcosceola.api.model.paciente.Paciente;
 import br.com.marcosceola.api.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
-    @PutMapping
+    @PostMapping
     public void cadastrar(@RequestBody PacienteForm pacienteForm) {
         pacienteService.save(new Paciente(pacienteForm));
     }
